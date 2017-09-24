3ad7e2bc8cdf4991334df45cbea0cf9245702921
gandhis1 <siddhartha.a.gandhi@gmail.com>
2017-09-24 14:36:43 -0400

    Handle pay periods

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import collections
 import json
+from datetime import date, timedelta
 
 import requests
+from dateutil.parser import parse
 
 
 def main():
@@ -19,6 +21,26 @@ def main():
 
     # Calculate the pay
     pay_scale = config["pay_scale"]
+
+    # Iterate through all pay period start dates
+    first_start_date = config["pay_period"]["first_start_date"]
+    pay_period_start_date = parse(first_start_date).date()
+    while(pay_period_start_date <= date.today()):
+        pay_period_end_date = pay_period_start_date + timedelta(days=14)
+        print("Pay Period {0}-{1}:"
+              .format(pay_period_start_date.strftime("%#m/%#d/%Y"),
+                      pay_period_end_date.strftime("%#m/%#d/%Y")))
+        pay_period_appointment_list = [
+            appt for appt in appointment_list
+            if (parse(appt["datetime"]).date() >= pay_period_start_date) and
+               (parse(appt["datetime"]).date() < pay_period_end_date)
+        ]
+        calculate_income(pay_period_appointment_list, pay_scale)
+        pay_period_start_date = pay_period_end_date
+        print("")
+
+
+def calculate_income(appointment_list, pay_scale):
     cumulative_payment = 0.0
     appointment_durations = []
     for appointment in appointment_list:
@@ -31,10 +53,12 @@ def main():
     appointment_count = collections.Counter(appointment_durations)
 
     # Print out the results
-    print("Total Income: $" + str(cumulative_payment))
+    print("  Total Income: $" + str(cumulative_payment))
+    print("  Total Appointments: " + str(len(appointment_list)))
     for duration, pay in pay_scale.items():
-        print("{0}: {1} at ${2} each"
-              .format(duration, appointment_count[duration], pay))
+        print("  {0}: {1} at ${2} each = ${3}"
+              .format(duration, appointment_count[duration], 
+                      pay, pay * appointment_count[duration]))
 
 
 if __name__ == '__main__':
