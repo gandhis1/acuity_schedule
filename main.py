ef4610c0e7299f7194fb9f9d7dfda519f34f0671
gandhis1 <siddhartha.a.gandhi@gmail.com>
2017-10-18 23:26:52 -0400

    Update email format to include number of  prior visits per client and number of appointments today

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import collections
 import json
+import smtplib
 from datetime import date, timedelta
+from itertools import groupby
 
 import requests
 from dateutil.parser import parse
-
-import smtplib
 
 message = "\n"
 
@@ -14,6 +14,21 @@ def log(msg):
     global message
     message += (msg + "\n")
     print(msg)
+
+
+def num_to_order(num):
+    num_str = str(num)
+    if num_str[-1] in ("0", "4", "5", "6", "7", "8", "9"):
+        suffix = "th"
+    elif num_str[-1] in ("1"):
+        suffix = "st"
+    elif num_str[-1] in ("2"):
+        suffix = "nd"
+    elif num_str[-1] in ("3"):
+        suffix = "rd"
+    else:
+        raise ValueError("The number passed must be an integer.")
+    return str(num) + suffix
 
 
 def main():
@@ -32,24 +47,35 @@ def main():
     response = requests.get(acuity_url, auth=(acuity_user_id, acuity_api_key))
     if response.status_code != 200:
         raise RuntimeError("Acuity API request failed")
-    appointment_list = response.json()
+    appt_list = response.json()
+
+    # Determine number of times each person has visited
+    def first_last_name_key(appt):
+        return "{0} {1}".format(appt["firstName"], appt["lastName"]).upper()
+    appt_groups = groupby(sorted(appt_list, key=first_last_name_key),
+                          key=first_last_name_key)
+    appt_count = {k: len(list(g)) for k, g in appt_groups}
 
     # Upcoming appointments
     log("Upcoming Appointments:\n")
-    upcoming_appts = [appt for appt in appointment_list
+    upcoming_appts = [appt for appt in appt_list
                       if parse(appt["datetime"]).date() >= date.today()]
     for appt in sorted(upcoming_appts, key=lambda x: parse(x["datetime"])):
         appt_start_datetime = (parse(appt["datetime"])
                                .strftime("%a %b %d %#I:%M"))
         appt_end_time = parse(appt["endTime"]).strftime("%#I:%M %p")
-        appt_name = "{0} {1}".format(appt["firstName"], appt["lastName"])
+        appt_name = "{0} {1}".format(appt["firstName"],
+                                     appt["lastName"]).title()
         appt_duration = appt["duration"]
         appt_reason = [x for x in appt["forms"][0]["values"]
-                       if x["fieldID"] == 2451841][0]["value"]
-        log("{0} - {1}: {2} ({3} min){4}"
+                       if x["fieldID"] == 2451841][0]["value"].strip()
+        num_appts_same_person = appt_count[appt_name.upper()]
+        log("{0} - {1}: {2} ({3} min){4}{5}"
             .format(appt_start_datetime, appt_end_time, appt_name,
                     appt_duration,
-                    " [{}]".format(appt_reason)
+                    ", {} visit".format(num_to_order(num_appts_same_person))
+                    if num_appts_same_person > 1 else "",
+                    ", {}".format(appt_reason)
                     if appt_reason.strip() != "" else ""))
 
     # Iterate through all pay period start dates
@@ -62,7 +88,7 @@ def main():
             .format(pay_period_start_date.strftime("%#m/%#d/%Y"),
                     pay_period_end_date.strftime("%#m/%#d/%Y")))
         pay_period_appointment_list = [
-            appt for appt in appointment_list
+            appt for appt in appt_list
             if (parse(appt["datetime"]).date() >= pay_period_start_date) and
                (parse(appt["datetime"]).date() < pay_period_end_date)
         ]
@@ -71,6 +97,9 @@ def main():
         log("")
 
     # Send an email
+    num_appts_today = len(list(x for x in upcoming_appts
+                               if parse(x["datetime"]).date() == date.today()))
+    email_subject += " ({} Appointments Today)".format(num_appts_today)
     email_text = ("From: {0}\nTo: {1}\nSubject: {2}\n\n{3}"
                   .format(gmail_user,
                           ", ".join(email_recipients),
